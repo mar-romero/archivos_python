57133475473ef2c28f19df43495d324eedc95d09
mar-romero <65137011+mar-romero@users.noreply.github.com>
2020-06-22 21:39:42 -0300

    Terminado
    Perdon no pude, vuelvo a tener problemas para poder ingresar 2 variable, opcion 1 o mpro para que ingrese a esa categoria y no solo 1 o el nombre

--- mar.py
+++ mar.py
@@ -4,25 +4,28 @@ def menu():
     print('2) M45-49')
     print('3) M25-29')
     print('4) M18-24')
+    #como se podria hacer para qu eliga el nombre o el numero cuando lo ingresa a la consola, solo pude por el numero de opcion
+    #ya que si ponia el nombre no me tomaba esa categoria, como se podria hacer?
+    
     while True: 
         try: 
-            cat = str(input('Ingrese la categoria analizar: '))
-            if cat == 1 or 'mpro' :
+            cat = int(input('Ingrese la categoria analizar: '))
+            if cat == 1 :
                 categoria = "MPRO"
                 print('Eligio categoria: ', cat,') MPRO',sep="")
                 return categoria
                 break
-            elif cat == 2 or 'm45-49' :
+            elif cat == 2 :
                 categoria = 'M45-49'
                 print('Eligio categoria: ', cat,') M45-49',sep="")
                 return categoria
                 break
-            elif cat == 3 or 'M25-29':
+            elif cat == 3 :
                 categoria = 'M25-29'
                 print('Eligio categoria: ', cat,') M25-29',sep="")
                 return categoria
                 break
-            elif cat == 4 or 'M18-24':
+            elif cat == 4 :
                 categoria = 'M18-24'
                 print('Eligio categoria: ', cat,') M18-24',sep="")
                 return categoria
