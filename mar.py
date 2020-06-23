e8c97e519cf8e735412bd594d15efbbb521eed5d
mar-romero <65137011+mar-romero@users.noreply.github.com>
2020-06-22 21:34:22 -0300

    Terminado
    Arregle una acotacion! me quedo solo una!

--- mar.py
+++ mar.py
@@ -4,27 +4,25 @@ def menu():
     print('2) M45-49')
     print('3) M25-29')
     print('4) M18-24')
-    #como se podria hacer para qu eliga el nombre o el numero cuando lo ingresa a la consola, solo pude por el numero de opcion
-    #ya que si ponia el nombre no me tomaba esa categoria, como se podria hacer?
     while True: 
         try: 
-            cat = int(input('Ingrese la categoria analizar: '))
-            if cat == 1 :
+            cat = str(input('Ingrese la categoria analizar: '))
+            if cat == 1 or 'mpro' :
                 categoria = "MPRO"
                 print('Eligio categoria: ', cat,') MPRO',sep="")
                 return categoria
                 break
-            elif cat == 2 :
+            elif cat == 2 or 'm45-49' :
                 categoria = 'M45-49'
                 print('Eligio categoria: ', cat,') M45-49',sep="")
                 return categoria
                 break
-            elif cat == 3 :
+            elif cat == 3 or 'M25-29':
                 categoria = 'M25-29'
                 print('Eligio categoria: ', cat,') M25-29',sep="")
                 return categoria
                 break
-            elif cat == 4 :
+            elif cat == 4 or 'M18-24':
                 categoria = 'M18-24'
                 print('Eligio categoria: ', cat,') M18-24',sep="")
                 return categoria
